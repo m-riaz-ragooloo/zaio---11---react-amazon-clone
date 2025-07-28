80da3780f73e091d9c9c40961c40ca9044194334
Riaz Ragooloo <riazragooloo@gmail.com>
2025-07-28 10:07:44 +0200

    added product dummy layout

--- amazon-clone/src/components/Home.jsx
+++ amazon-clone/src/components/Home.jsx
@@ -1,4 +1,6 @@
 import { Outlet } from "react-router-dom";
+import './Home.css'
+import Products from "./Products"
 
 // export default function Home(){
 //     return(
@@ -14,8 +16,9 @@ export default function Home() {
     <main>
       <div className="home">
         <div className="home-container">
-            <img src="https://m.media-amazon.com/images/I/612ouh5unhL._SX3000_.jpg" alt="Hero image" />
+            <img className="home-image" src="https://m.media-amazon.com/images/I/612ouh5unhL._SX3000_.jpg" alt="Hero image" />
           <Outlet />
+          <Products />
         </div>
       </div>
     </main>
--- amazon-clone/src/components/Products.jsx
+++ amazon-clone/src/components/Products.jsx
@@ -1,14 +1,58 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "./Products.css";
+import Product from "./Product";
 
 export default function Products() {
-    return (
-        <main>
-            <h1>Product List</h1>
-            <ul>
-                <li><Link to="/products/01">Product 1</Link></li>
-                <li><Link to="/products/02">Product 2</Link></li>
-                <li><Link to="/products/03">Product 3</Link></li>
-            </ul>
-        </main>
-    )
+  return (
+    <main>
+      <div className="product-row">
+        <Product
+          id="1"
+          title="Norden Portable Adjustable Laptop Stand – Aluminum Alloy Folding Computer Stand laptop stand"
+          image="https://m.media-amazon.com/images/I/31XS32Ytx5L._SR480,440_.jpg"
+          rating="4.3"
+          price="R97.00"
+        />
+        <Product
+          id="2"
+          title="Logitech G502 Lightspeed Wireless Gaming Mouse, Black, One Size"
+          image="https://m.media-amazon.com/images/I/61t-YGvswzL._AC_SY550_.jpg"
+          rating="4.6"
+          price="R1799.00"
+        />
+      </div>
+      <div className="product-row">
+        <Product
+          id="1"
+          title="Norden Portable Adjustable Laptop Stand – Aluminum Alloy Folding Computer Stand laptop stand"
+          image="https://m.media-amazon.com/images/I/31XS32Ytx5L._SR480,440_.jpg"
+          rating="4.3"
+          price="R97.00"
+        />
+        <Product
+          id="2"
+          title="Logitech G502 Lightspeed Wireless Gaming Mouse, Black, One Size"
+          image="https://m.media-amazon.com/images/I/61t-YGvswzL._AC_SY550_.jpg"
+          rating="4.6"
+          price="R1799.00"
+        />
+        <Product
+          id="2"
+          title="Logitech G502 Lightspeed Wireless Gaming Mouse, Black, One Size"
+          image="https://m.media-amazon.com/images/I/61t-YGvswzL._AC_SY550_.jpg"
+          rating="4.6"
+          price="R1799.00"
+        />
+      </div>
+      <div className="product-row">
+        <Product
+          id="1"
+          title="Norden Portable Adjustable Laptop Stand – Aluminum Alloy Folding Computer Stand laptop stand"
+          image="https://m.media-amazon.com/images/I/31XS32Ytx5L._SR480,440_.jpg"
+          rating="4.3"
+          price="R97.00"
+        />
+        </div>
+    </main>
+  );
 }
